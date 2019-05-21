fbcb8fabcbdc439e73ab6859950401f481793bd9
Dina Farouk <dina.farouk799@gmail.com>
2019-05-21 12:42:54 +0200

    imperial

--- Imperial/js/custom.js
+++ Imperial/js/custom.js
@@ -107,6 +107,19 @@ jQuery(document).ready(function( $ ) {
       return false;
   });
 
+  
+
+
+
+
+
+//(".portfolio-modal").on("show.bs.modal",function(a){o(".navbar").addClass("d-none")}),o(".portfolio-modal").on("hidden.bs.modal",function(a){o(".navbar").removeClass("d-none")});
+
+
+
+
+
+
 });
 var pJS = function(tag_id, params){
 
@@ -1642,117 +1655,240 @@ var pJS = function(tag_id, params){
   };
   
 
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 2,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 150,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
-  });
+
+
+
+
+
+
+
+
+
+
+// particlesJS("particles-js", {
+//     "particles": {
+//       "number": {
+//         "value": 80,
+//         "density": {
+//           "enable": true,
+//           "value_area": 800
+//         }
+//       },
+//       "color": {
+//         "value": "#ffffff"
+//       },
+//       "shape": {
+//         "type": "circle",
+//         "stroke": {
+//           "width": 0,
+//           "color": "#000000"
+//         },
+//         "polygon": {
+//           "nb_sides": 5
+//         },
+//         "image": {
+//           "src": "img/github.svg",
+//           "width": 100,
+//           "height": 100
+//         }
+//       },
+//       "opacity": {
+//         "value": 0.5,
+//         "random": false,
+//         "anim": {
+//           "enable": false,
+//           "speed": 1,
+//           "opacity_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "size": {
+//         "value": 2,
+//         "random": true,
+//         "anim": {
+//           "enable": false,
+//           "speed": 40,
+//           "size_min": 0.1,
+//           "sync": false
+//         }
+//       },
+//       "line_linked": {
+//         "enable": true,
+//         "distance": 150,
+//         "color": "#ffffff",
+//         "opacity": 0.4,
+//         "width": 1
+//       },
+//       "move": {
+//         "enable": true,
+//         "speed": 2,
+//         "direction": "none",
+//         "random": false,
+//         "straight": false,
+//         "out_mode": "out",
+//         "bounce": false,
+//         "attract": {
+//           "enable": false,
+//           "rotateX": 600,
+//           "rotateY": 1200
+//         }
+//       }
+//     },
+//     "interactivity": {
+//       "detect_on": "canvas",
+//       "events": {
+//         "onhover": {
+//           "enable": true,
+//           "mode": "grab"
+//         },
+//         "onclick": {
+//           "enable": true,
+//           "mode": "push"
+//         },
+//         "resize": true
+//       },
+//       "modes": {
+//         "grab": {
+//           "distance": 150,
+//           "line_linked": {
+//             "opacity": 1
+//           }
+//         },
+//         "bubble": {
+//           "distance": 400,
+//           "size": 40,
+//           "duration": 2,
+//           "opacity": 8,
+//           "speed": 3
+//         },
+//         "repulse": {
+//           "distance": 200,
+//           "duration": 0.4
+//         },
+//         "push": {
+//           "particles_nb": 4
+//         },
+//         "remove": {
+//           "particles_nb": 2
+//         }
+//       }
+//     },
+//     "retina_detect": true
+//   });
   
-  requestAnimationFrame();
+//   requestAnimationFrame();
   
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   var canvas = document.createElement('canvas');
+// canvas.width = document.body.clientWidth;
+// canvas.height = document.body.clientHeight;
+// document.body.appendChild(canvas);
+
+// var ctx = canvas.getContext('2d');
+
+// var mousePos = { x: 0, y: 0 };
+// window.onmousemove = function(e) {
+//   mousePos.x = e.clientX;
+//   mousePos.y = e.clientY;
+// }
+// function distanceFromCenter() {
+//   return Math.sqrt(Math.pow(mousePos.x - (canvas.width / 2), 2) + Math.pow(mousePos.y - (canvas.height / 2), 2));
+// }
+
+// function Particle() {
+//   this.theta = Math.random() * Math.PI * 2;
+//   this.radius = (Math.random() * ((canvas.width > canvas.height) ? canvas.width : canvas.height) * 0.33) + 40;
+//   this.maxRadius = (Math.random() * ((canvas.width > canvas.height) ? canvas.width : canvas.height) * 0.45);
+//   this.radialChange = Math.random() * 0.1 *  (Math.random() > 0.5) ? 1 : -1;
+//   this.opacity = Math.random();
+//   this.size = Math.round(Math.random() * 6) + 4;
+//   this.speed = Math.round(Math.random() * 4) + 1;
+//   this.direction = (Math.random() > 0.5) ? 1 : -1;
+//   this.x = 0;
+//   this.y = 0;
+//   this.connected = (Math.random() < 0.75);
+// }
+// Particle.prototype.update = function() {
+//   this.theta += this.speed / 750 * this.direction;
+//   this.x = (canvas.width / 2) + (Math.cos(this.theta) * this.radius) * (distanceFromCenter() / this.maxRadius);
+//   this.y = (canvas.height / 2) + (Math.sin(this.theta) * this.radius) * (distanceFromCenter() / this.maxRadius);
+//   this.radius += this.radialChange;
+//   if (Math.abs(this.radius) > this.maxRadius) {
+//     this.radialChange *= -1;
+//   }
+// };
+// Particle.prototype.render = function() {
+//   ctx.save();
+//   ctx.beginPath();
+//   ctx.fillStyle = 'white';
+//   ctx.strokeStyle = '#444';
+//   ctx.globalAlpha = this.opacity;
+//   ctx.arc(this.x, this.y, this.size / 2, 0, 2 * Math.PI, false);
+//   ctx.fill();
+//   ctx.stroke();
+//   ctx.restore();
+// };
+
+// var particles = [];
+// for(var i = 0; i < (Math.random() * 50) + 100; i++) {
+//   particles.push(new Particle());
+// }
+// requestAnimationFrame(demo = function() {
+//   ctx.save();
+//   ctx.fillStyle = '#242424';
+//   ctx.fillRect(0, 0, canvas.width, canvas.height);
+//   ctx.restore();
+  
+//   particles.forEach(function(particle, i) {
+//     ctx.lineTo(particle.x, particle.y);
+//     particle.update();
+//     particle.render();
+//     if (particle.connected) {
+//       var p2 = particles[i + 1];
+//       if (p2) {
+//         ctx.save();
+//         ctx.beginPath();
+//         ctx.strokeStyle = '#fff';
+//         ctx.globalAlpha = particle.opacity * 0.33;
+//         ctx.moveTo(particle.x, particle.y);
+//         ctx.lineTo(p2.x, p2.y);
+//         ctx.stroke();
+//         ctx.restore();
+//       }
+//     }
+//   });
+  
+//   requestAnimationFrame(demo);
+// });
